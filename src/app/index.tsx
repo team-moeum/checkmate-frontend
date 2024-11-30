c42bc32ed9a48dbdbb64631a39984441bca7cc69
mac <mac@macui-MacBookAir.local>
2024-11-30 19:16:21 +0900

    zustand 추가

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,23 @@
+import { Button } from "react-native";
 import { Text, View } from "react-native";
+import {create} from 'zustand'
+
+type Store = {
+  count:number
+  inc:()=>void
+}
+const useStore = create<Store>((set) => ({
+  count: 1,
+  inc: () => set((state) => ({ count: state.count + 1 })),
+}))
+
 
 export default function HomeScreen() {
+  const { count, inc } = useStore()
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
+      <Text>{count}</Text>
+      <Button title="one up" onPress={inc}></Button>
     </View>
   );
 }

d29a05462b625038e5b53c1312f0831bfbfce5be
seungdeok <akk9132@naver.com>
2024-12-29 22:23:42 +0900

    fix: fix lint error

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,4 @@
 import { Tabs } from "expo-router";
-import '../../../A/global.css'
 
 export default function RootLayout() {
   return (
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,27 @@
 import { Text, View } from "react-native";
-import useSWR from 'swr'
+import useSWR from "swr";
 
-const fetcher = async(path:string) => {
+const fetcher = async (path: string) => {
   const res = await fetch(path);
   const data = await res.json();
   return data;
-}
+};
 
 export default function HomeScreen() {
-    const { data, error, isLoading } = useSWR('/api/user/123', fetcher)
-    if(error) return <View><Text>error</Text></View>
-    if(isLoading) return <View><Text>loading</Text></View>
+  const { data, error, isLoading } = useSWR("/api/user/123", fetcher);
+  if (error)
     return (
+      <View>
+        <Text>error</Text>
+      </View>
+    );
+  if (isLoading)
+    return (
+      <View>
+        <Text>loading</Text>
+      </View>
+    );
+  return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>{data}</Text>
     </View>

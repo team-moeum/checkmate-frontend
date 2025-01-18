7ba864ffd8b9587330978fbcaa1a96bf861de19d
seungdeok <akk9132@naver.com>
2025-01-18 14:34:03 +0900

    refactor: change navigation(bottom-tabs to stack)

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,31 +1,10 @@
-import { Text, View } from "react-native";
-import useSWR from "swr";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { HomeScreen } from "@moeum/features/home/HomeScreen";
 
-const fetcher = async (path: string) => {
-  const res = await fetch(path);
-  const data = await res.json();
-  return data;
-};
-
-export default function HomeScreen() {
-  const { data, error, isLoading } = useSWR("/api/user/123", fetcher);
-  if (error)
-    return (
-      <View>
-        <Text>error</Text>
-      </View>
-    );
-  if (isLoading)
-    return (
-      <View>
-        <Text>Home Screen</Text>
-        <Text>loading</Text>
-      </View>
-    );
+export default function Home() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>{data}</Text>
-      <Text>Home Screen</Text>
-    </View>
+    <SafeAreaView>
+      <HomeScreen />
+    </SafeAreaView>
   );
 }

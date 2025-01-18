2df8f6b347c9330be2577aea5e7336bd1a380714
seungdeok <akk9132@naver.com>
2025-01-18 15:41:45 +0900

    feat: add Header in Home

--- src/features/home/HomeScreen.tsx
+++ src/features/home/HomeScreen.tsx
@@ -7,19 +7,30 @@ import { Asset } from "@moeum/features/home/components/Asset";
 import { LinearProgress } from "@moeum/features/home/components/Progress/LinearProgress";
 import { TodoList } from "@moeum/features/home/components/Todo/TodoList";
 import { FAB } from "@moeum/features/home/components/FAB";
+import { StyleSheet, View } from "react-native";
 
 export const HomeScreen = () => {
   return (
     <ErrorBoundary>
-      <Header />
-      <Nav />
-      <CircleProgress start={1} end={5} />
-      <Asset />
-      <LinearProgress start={1} end={100} />
-      <Suspense>
-        <TodoList />
-      </Suspense>
-      <FAB onPress={() => {}} />
+      <View style={styles.container}>
+        <Header />
+        <Nav />
+        <CircleProgress start={1} end={5} />
+        <Asset />
+        <LinearProgress start={1} end={100} />
+        <Suspense>
+          <TodoList />
+        </Suspense>
+        <FAB onPress={() => {}} />
+      </View>
     </ErrorBoundary>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    position: "relative"
+  }
+});
--- src/features/home/components/Header.tsx
+++ src/features/home/components/Header.tsx
@@ -1,5 +1,35 @@
-import { View } from "react-native";
-
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { showNotification } from "@moeum/features/home/utils/showNotification";
 export const Header = () => {
-  return <View></View>;
+  return (
+    <View style={styles.header}>
+      <Image source={require("assets/images/logo.png")} style={styles.logo} />
+      <TouchableOpacity
+        onPress={() => {
+          showNotification("마이페이지입니다");
+        }}
+      >
+        <Text style={styles.buttonText}>마이</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  header: {
+    height: 56,
+    paddingHorizontal: 16,
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  logo: {
+    width: 100,
+    height: 14,
+    resizeMode: "contain"
+  },
+  buttonText: {
+    fontSize: 16,
+    color: "#494F54"
+  }
+});

d1d4ebffe1a5490bf2d98d89ea5119630e3e6943
seungdeok <akk9132@naver.com>
2025-01-19 15:37:14 +0900

    refactor: change SafeAreaView position

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,5 @@
-import { SafeAreaView } from "react-native-safe-area-context";
 import { HomeScreen } from "@moeum/features/home/HomeScreen";
 
 export default function Home() {
-  return (
-    <SafeAreaView>
-      <HomeScreen />
-    </SafeAreaView>
-  );
+  return <HomeScreen />;
 }

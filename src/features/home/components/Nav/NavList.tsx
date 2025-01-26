ab60bacd6d0eec827c6a62bbf956abcd0d3eaf57
seungdeok <akk9132@naver.com>
2025-01-26 16:48:04 +0900

    style: change item gap in NavList

--- src/features/home/components/Nav/NavList.tsx
+++ src/features/home/components/Nav/NavList.tsx
@@ -1,39 +1,7 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { NavItem } from "./NavItem";
+import { NAV_ITEMS } from "./constants";
 import { showNotification } from "@moeum/features/home/utils/showNotification";
-
-const NAV_ITEMS = [
-  {
-    image: require("assets/images/nav/bronze.png"),
-    label: "도전 등급",
-    description: "브론즈"
-  },
-  {
-    image: require("assets/images/nav/point.png"),
-    label: "포인트",
-    description: "12코인"
-  },
-  {
-    image: require("assets/images/nav/calendar.png"),
-    label: "출석부",
-    description: "5일차"
-  },
-  {
-    image: require("assets/images/nav/goal.png"),
-    label: "도전 목록",
-    description: ""
-  },
-  {
-    image: require("assets/images/nav/shop.png"),
-    label: "상점",
-    description: ""
-  },
-  {
-    image: require("assets/images/nav/alarm.png"),
-    label: "알림",
-    description: ""
-  }
-];
 
 export const NavList = () => {
   return (
@@ -60,6 +28,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     paddingVertical: 8,
     height: 108,
-    backgroundColor: "white"
+    backgroundColor: "white",
+    justifyContent: "space-between"
   }
 });
---
+++ src/features/home/components/Nav/constants.ts
@@ -0,0 +1,32 @@
+export const NAV_ITEMS = [
+  {
+    image: require("assets/images/nav/bronze.png"),
+    label: "도전 등급",
+    description: "브론즈"
+  },
+  {
+    image: require("assets/images/nav/point.png"),
+    label: "포인트",
+    description: "12코인"
+  },
+  {
+    image: require("assets/images/nav/calendar.png"),
+    label: "출석부",
+    description: "5일차"
+  },
+  {
+    image: require("assets/images/nav/goal.png"),
+    label: "도전 목록",
+    description: ""
+  },
+  {
+    image: require("assets/images/nav/shop.png"),
+    label: "상점",
+    description: ""
+  },
+  {
+    image: require("assets/images/nav/alarm.png"),
+    label: "알림",
+    description: ""
+  }
+];
